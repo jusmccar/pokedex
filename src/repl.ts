885f697be49dfba83c14620f13f37e6fb214308a
jusmccar <justinmccartney1@yahoo.com>
2026-01-13 19:37:46 -0800

    Add startREPL function

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,31 @@
+import readline from "node:readline";
+
 export function cleanInput(input: string): string[] {
 	return input.trim().toLowerCase().split(/\s+/);
+}
+
+export function startREPL() {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		prompt: "Pokedex > ",
+	});
+
+	rl.prompt();
+
+	rl.on("line", (line) => {
+		const words = cleanInput(line);
+
+		if (words.length === 0) {
+			rl.prompt();
+			return;
+		}
+
+		console.log(`Your command was: ${words[0]}`);
+		rl.prompt();
+	});
+
+	rl.on("close", () => {
+		process.exit(0);
+	});
 }
